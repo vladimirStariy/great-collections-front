3c171d18dfe93001c4ebba2a469627e47fcb6492
vladimirStary <puffychords@gmail.com>
2023-11-03 18:47:57 +0300

    initial

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+);
