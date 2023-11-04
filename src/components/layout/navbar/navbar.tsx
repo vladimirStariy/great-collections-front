3e41faf8f13c98646e7b6828d3d90eb105f312f2
vladimirStary <puffychords@gmail.com>
2023-11-04 12:12:53 +0300

    Actions fix warnings

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -9,17 +9,17 @@ const NavigationBar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Homepage</a></li>
-                        <li><a>Portfolio</a></li>
-                        <li><a>About</a></li>
+                        <li>Homepage</li>
+                        <li>Portfolio</li>
+                        <li>About</li>
                     </ul>
                 </div>
-                <a>Homepage</a>
-                <a>Portfolio</a>
-                <a>About</a>
+                Homepage
+                Portfolio
+                About
             </div>
             <div className="navbar-center">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <div className="btn btn-ghost normal-case text-xl">daisyUI</div>
             </div>
             <div className="navbar-end">
                 <button className="btn btn-ghost btn-circle">
