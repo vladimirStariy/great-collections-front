18c29fc93ba5b358e20d6d21222017330a4026fa
vladimirStary <puffychords@gmail.com>
2023-11-07 21:51:51 +0300

    Auth and roles access 3

--- src/components/layout/test.screen.tsx
+++ src/components/layout/test.screen.tsx
@@ -1,5 +1,10 @@
 const TestScreen = () => {
+    const handleCheck = () => {
+        console.log(process.env.REACT_APP_BASE_URL)
+    }
+    
     return <>
+        <button onClick={handleCheck}>asd</button>
     </>
 }
 
