fa714db12e410205612ee19d7f24d8812e99238c
vladimirStary <puffychords@gmail.com>
2023-11-22 16:47:40 +0300

    web socket implementation

--- src/components/collection/collection-page/collection.item.screen.tsx
+++ src/components/collection/collection-page/collection.item.screen.tsx
@@ -1,4 +1,4 @@
-import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
+import { BreadcrumbItem, Breadcrumbs, Button, Textarea } from "@nextui-org/react";
 import { useGetCollectionItemQuery } from "../../../store/services/collection.service";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -13,30 +13,46 @@ const CollectionItemPage = () => {
 
     useEffect(() => {
         if(isSuccess) setCollectionItemData(itemData)
+        console.log(itemData)
     }, [itemData])
 
     return <>
-        <Breadcrumbs size='lg' className="px-4 pb-8" underline='hover'>
+        <Breadcrumbs size='lg' className="px-4 pb-4" underline='hover'>
             <BreadcrumbItem href="/collections">Collections</BreadcrumbItem>
             <BreadcrumbItem href={`/collection/${collectionItemData?.collection_id}`}>{collectionItemData?.collection.name}</BreadcrumbItem>
             <BreadcrumbItem>{collectionItemData?.name}</BreadcrumbItem>
         </Breadcrumbs>
-        <div className="w-full flex justify-center bg-gradient-to-r from-fuchsia-500 to-violet-500 p-8 rounded-lg">
-            <div className='flex flex-col w-full max-w-screen-2xl gap-4 justify-start items-start'>
-                <div className="flex flex-col text-white gap-4">
-                    <div className="text-3xl font-black text-bold ">
-                        {collectionItemData?.name.toUpperCase()}
+        <div className="flex flex-col w-full gap-4">
+            <div className="w-full flex justify-center bg-gradient-to-r from-fuchsia-500 to-violet-500 p-8 rounded-xl">
+                <div className='flex flex-col w-full max-w-screen-2xl gap-4 justify-start items-start'>
+                    <div className="flex flex-col text-white gap-4">
+                        <div className="text-3xl font-black text-bold ">
+                            {collectionItemData?.name.toUpperCase()}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className='flex flex-col w-full justify-start pt-4 px-4'>
-            <div>Provider: Jhonny Grace</div>
-            <div>Goal: big hit</div>
-            <div>Provider: Jhonny Grace</div>
-            <div>Goal: big hit</div>
-            <div>Provider: Jhonny Grace</div>
-            <div>Goal: big hit</div>
+            <div className='flex flex-col w-full justify-start px-4'>
+                {collectionItemData ? <>
+                    {collectionItemData.collectionFields.map((item, index) => {
+                        return <div>{item.name}:{item.value}</div>
+                    })}
+                </> : <>
+                
+                </>}
+            </div>
+            <div className="w-full flex flex-col gap-4">
+                <Textarea
+                    variant="bordered"
+                    maxRows={10}
+                    maxLength={600}
+                    label="Comment"
+                    placeholder="Put your comment here..."
+                />
+                <div className="flex flex-row w-full justify-end gap-4">
+                    <Button variant='bordered'>Send</Button>
+                </div>
+            </div>
         </div>
     </>
 }
--- src/components/layout/test.screen.tsx
+++ src/components/layout/test.screen.tsx
@@ -1,11 +1,59 @@
-import { ChangeEvent, FormEvent, useState } from "react"
-import { useCreateCollectionMutation } from "../../store/services/collection.service"
-import { ICreateCollectionRequest } from "../../store/models/collection";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
+
+import { io, Socket } from 'socket.io-client'
+import { Comment } from "./socket.interfaces";
+import { Button, Input } from "@nextui-org/react";
+
+interface IUserComment {
+    collectionItemId: number,
+    userId: number,
+    text: string
+}
+
+const socket: Socket = io('http://localhost:5000/');
 
 const TestScreen = () => {
 
-    return <>
+    const [comments, setComments] = useState<string[]>([]);
+    const [user, setUser] = useState<string>("Vova");
+    const [comment, setComment] = useState<string>('')
 
+    const handleChangeValue = (text: string) => {
+        setComment(text)
+    }
+    
+    useEffect(() => {
+        socket.on('comment', (e) => setComments((prev) => [...prev, e]));
+        return () => {
+            socket.off('comment');
+        }
+    }, [socket, comments]);
+
+    useEffect(() => {
+        return () => {
+            socket.disconnect();
+        };
+    }, [])
+
+    const sendMessage = (e: FormEvent<HTMLButtonElement> | FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        socket.emit('comment', comment)
+        setComment('');
+    };
+
+    return <>
+        <div>
+            {comments && comments.map((item) => (
+                <div>{item}</div>
+            ))}
+        </div>
+        <form onSubmit={(e) => sendMessage(e)}>
+            <Input 
+                value={comment}
+                onChange={(e) => handleChangeValue(e.target.value)}
+            />
+            <Button type="submit" onSubmit={(e) => sendMessage(e)}>WHA</Button>
+        </form>
     </>
 }
 
